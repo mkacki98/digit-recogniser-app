19f01aede26ceab6c3bbab4ac18c1b400398069f
Miko <mikolajkacki98@gmail.com>
2023-02-18 20:32:13 +0000

    changes in request and image format

--- app.py
+++ app.py
@@ -1,28 +1,36 @@
-from flask import Flask, render_template, request, jsonify
 import base64
-from torch.nn.functional import normalize
 import cv2
 import numpy as np
 import torch
+
+from torch.nn.functional import normalize
+from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 def get_image(canvas):
+    """ Decode the canvas and pass it as OpenCV image. """
+
     decoded_canvas = base64.b64decode(canvas.split(',')[1].encode())
     canvas_as_np = np.frombuffer(decoded_canvas, dtype=np.uint8)
+    
     img = cv2.imdecode(canvas_as_np, flags=1)
-
     resized_img = cv2.resize(img,(28,28))
-    cv2.imwrite('image_resized.jpg', resized_img)
     
     return resized_img
 
 def predict_image(image):
-    model = torch.load(f"models/mnist_classifier_base")
+    """ Load a model and use it to predict the image. """
 
-    image = image[:,:,0].flatten() # (784,)
-    image = (torch.from_numpy(image) / 255.0).reshape(1, 784)
+    model = torch.load(f"models/2cl-1fc_bs-64_lr-0.001_epoch-10")
+    model.eval()
+
+    image = image[:,:,0] # (28,28)
+    image = (torch.from_numpy(image)/1.0)  
+    image = torch.unsqueeze(torch.unsqueeze(image, 0), 0) # (1,1,28,28)
 
     predictions = model(normalize(image))
+
     return predictions, torch.argmax(predictions)
 
 @app.route("/predict", methods = ["GET"])
